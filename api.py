0652513c81b40d1e0d9d6597c9cac9f809d95513
H4uMichBl4u <hk-misc@web.de>
2023-12-11 18:06:31 +0100

    test_api_request (kürzer)

--- api.py
+++ api.py
@@ -1,12 +1,11 @@
 import requests
 
+comp_url = "http://141.41.235.28/JSON_Kompressor_IPT/Kompressor_Json.php"
 def test_api_request_successful():
-    comp_url = "http://141.41.235.28/JSON_Kompressor_IPT/Kompressor_Json.php"
     response = requests.get(comp_url)
     assert response.status_code == 200
 
 def test_api_request_returns_json():
-    comp_url = "http://141.41.235.28/JSON_Kompressor_IPT/Kompressor_Json.php"
     response = requests.get(comp_url)
     assert response.headers['Content-Type'] == 'application/json'
 
