be4cc9c6c3790d0989a903501c3f848212838e3f
annikazink <annika.zink@icloud.com>
2023-12-14 11:25:06 +0100

    Datenbank Verbindung - aber funktioniert noch nicht

---
+++ GUI.py
@@ -0,0 +1,20 @@
+# Verbindung zur Datenbank
+# funktioniert noch nicht
+def connect_to_database():
+    try:
+        connection = pymysql.connect(
+            host="141.41.42.211",
+            user="kompressor",
+            password="InfoLabor_Gr2",
+            database="kompressor",
+            charset="utf8mb4",
+            cursorclass=pymysql.cursors.DictCursor,
+        )
+        return connection
+    except pymysql.Error as e:
+        print(f"Error connecting to the database: {e}")
+        return None
+
+
+connect_to_database()
+
