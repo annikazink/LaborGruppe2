93dd4002a5a6e5584b35a32ace25575025fe6aea
martenweiser <martenweiser@gmail.com>
2023-12-15 15:25:01 +0100

    Diagramm mit 7 tage und 24h ansicht

--- diagramm test.py
+++ diagramm test.py
@@ -3,9 +3,17 @@ import matplotlib.pyplot as plt
 import random
 import datetime
 
-def create_temperature_chart():
-    # Hier kommt der Code zur Erstellung des Diagramms
-    hours = int(input("Wie viele Stunden?"))
+def create_temperature_options():
+    # Funktion, die aufgerufen wird, wenn der "Temperatur" -Button geklickt wird
+    temperature_button.pack_forget()  # Verstecke den "Temperatur" -Button
+    text_label.config(text="Wählen Sie die Option aus:")  # Ändere die Beschriftung
+
+    # Erstelle die beiden Optionen (24 Stunden und 7 Tage)
+    button_24h.pack()
+    button_7d.pack()
+
+def create_temperature_chart(hours):
+    # Funktion zur Erstellung des Diagramms basierend auf der ausgewählten Option
     timestamps = []
     temperatures = []
 
@@ -19,19 +27,32 @@ def create_temperature_chart():
     plt.plot(timestamps, temperatures, marker='o', linestyle='-')
     plt.xlabel('Zeit')
     plt.ylabel('Temperatur (°C)')
-    plt.title('24-Stunden-Temperaturverlauf')
+    plt.title(f'{hours} Stunden Temperaturverlauf')
     plt.xticks(rotation=45)
     plt.tight_layout()
 
     plt.show()
 
+def create_24h_chart():
+    create_temperature_chart(24)
+
+def create_7d_chart():
+    create_temperature_chart(24 * 7)
+
 # Erstelle ein tkinter-Fenster
 root = tk.Tk()
 root.title("Temperaturdiagramm")
 
-# Erstelle einen Button für das Temperaturdiagramm
-temperature_button = tk.Button(root, text="Temperatur", command=create_temperature_chart)
+text_label = tk.Label(root, text="Klicken Sie auf 'Temperatur' um fortzufahren.")
+text_label.pack()
+
+# Erstelle den "Temperatur" -Button
+temperature_button = tk.Button(root, text="Temperatur", command=create_temperature_options)
 temperature_button.pack()
+
+# Erstelle die Buttons für 24 Stunden und 7 Tage (initial versteckt)
+button_24h = tk.Button(root, text="24 Stunden", command=create_24h_chart)
+button_7d = tk.Button(root, text="7 Tage", command=create_7d_chart)
 
 # Starte die tkinter-Hauptloop
 root.mainloop()
