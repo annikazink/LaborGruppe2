1ff46d3e1e47e7975cc17b9c9c4b764549aae9f4
annikazink <annika.zink@icloud.com>
2023-12-18 17:27:01 +0100

    Fehler bei historischen Daten behoben. 3 Pylint Fehler müssen noch behoben werden

--- gui_entwurf.py
+++ gui_entwurf.py
@@ -1,3 +1,9 @@
+"""
+Aufbereitung der Daten von Kompressoren in einer GUI
+
+Author: A. Zink, M. Weiser, C.
+Last modified: 18.12.23
+"""
 import tkinter as tk
 from tkinter import ttk
 from datetime import datetime, timedelta
@@ -156,26 +162,62 @@ class MainApp(tk.Tk):
         filtered_data = self.filter_data_by_device_id(data_array, device_id)
         filtered_data = self.filter_data_by_period(filtered_data, days)
         self.plot_data(filtered_data, x_label, y_label, title, x_key, y_key)
+
     def filter_and_plot_historische_daten(self, data_array, config):
         """
-            Kombiniert das Filtern der Daten nach Geräte-ID und Zeitraum
-             mit der Erstellung eines Diagramms.
-            """
+        Kombiniert das Filtern der Daten nach Geräte-ID und Zeitraum
+        mit der Erstellung eines Diagramms.
+        """
         device_id = config.get('device_id')
-        x_label = config.get('x_label', '')
-        y_label = config.get('y_label', '')
-        title = config.get('title', '')
+        x_label = config.get('x_label', 'Zeit')
+        y_label = config.get('y_label', 'Wert')
+        title = config.get('title', 'Diagramm')
         x_key = config.get('x_key', 'zeitstempel')
         y_key = config.get('y_key', '')
-        print(data_array)
-        filtered_data = self.filter_data_by_device_id(data_array, device_id)
-        print(filtered_data)
-        self.plot_data(filtered_data, x_label, y_label, title, x_key, y_key)
+
+        # Filtern der Daten basierend auf der Geräte-ID
+        filtered_data = [record for record in data_array if record.get('geraet_id') == device_id]
+
+        # Daten sortieren nach Zeitstempel
+        filtered_data.sort(key=lambda x: datetime.strptime(x.get(x_key, ''), '%Y-%m-%d %H:%M:%S'))
+
+        # Extrahieren von X- und Y-Daten
+        x_data = [datetime.strptime(record.get(x_key, ''), '%Y-%m-%d %H:%M:%S')
+                  for record in filtered_data]
+        y_data = [record.get(y_key, 0) for record in filtered_data]
+
+        # Diagramm erstellen
+        plt.figure(figsize=(10, 6))
+        plt.plot(x_data, y_data, marker='o', linestyle='-', color='b')
+        plt.xlabel(x_label)
+        plt.ylabel(y_label)
+        plt.title(title)
+
+        # Anpassung der X-Achse
+        plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+        plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=1))  # Jeden Tag markieren
+        plt.xticks(rotation=45)
+
+        plt.grid()
+        plt.tight_layout()
+        plt.show()
 
     def plot_gesamt_energie(self, gesamt_energie_daten, days):
-        filtered_data=self.filter_data_by_period(gesamt_energie_daten, days)
+        """
+           Plottet die Gesamtenergie über die angegebenen Tage.
+
+           Diese Methode nimmt Energieverbrauchsdaten und eine Zeitspanne in Tagen entgegen,
+            filtert die Daten
+           für den angegebenen Zeitraum und erstellt einen Plot,
+            der den Gesamtenergieverlauf über diesen Zeitraum zeigt.
+
+           """
+
+        filtered_data = self.filter_data_by_period(gesamt_energie_daten, days)
+
         # Extrahiere Zeitstempel und gesamt_energie aus den Datensätzen
-        zeitstempel = [datensatz['zeitstempel'] for datensatz in filtered_data]
+        zeitstempel = [datetime.strptime(datensatz['zeitstempel'], '%Y-%m-%d %H:%M:%S')
+                       for datensatz in filtered_data]
         gesamt_energie = [datensatz['gesamt_energie'] for datensatz in filtered_data]
 
         # Plot erstellen
@@ -184,10 +226,16 @@ class MainApp(tk.Tk):
         plt.title('Gesamtenergieverlauf')
         plt.xlabel('Zeitstempel')
         plt.ylabel('Gesamtenergie')
-        plt.grid(True)
-        plt.tight_layout()
 
-        # Plot anzeigen
+        # Anpassung der X-Achse
+        plt.gca().xaxis.set_major_formatter(
+            plt.matplotlib.dates.DateFormatter('%Y-%m-%d\n%H:%M'))  # Formatierung der Zeitstempel
+        # Intervall der Zeitstempel
+        plt.gca().xaxis.set_major_locator(plt.matplotlib.dates.DayLocator(interval=1))
+        plt.xticks(rotation=45)  # Rotation der Zeitstempel
+
+        plt.grid(True)
+        plt.tight_layout()  # Sorgt für eine ordentliche Anordnung der Plot-Elemente
         plt.show()
 
     def show_kompressor_ipt_kompressor(self):
@@ -629,7 +677,10 @@ class MainApp(tk.Tk):
         ).pack(pady=5, padx=10, side=tk.LEFT)
 
     def show_gesamt_energie(self):
-        # Präsentiert die Benutzeroberfläche für die Gesamtenergieansicht des Kompressors
+        """
+           Präsentiert die Benutzeroberfläche für die Ansicht des Gesamtenergieverbrauchs
+           eines Kompressors.
+           """
         self.close_current_menu()
         gesamt_ipt_energie_frame = tk.Frame(self)
         gesamt_ipt_energie_frame.pack(padx=10, pady=10)
