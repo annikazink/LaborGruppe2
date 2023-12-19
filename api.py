cfbce33c51fcdf99848faaf871db221039ec18fd
H4uMichBl4u <hk-misc@web.de>
2023-12-19 13:17:10 +0100

    api.py OOP-Umwandlung + test_api.py Neuauflage Entwurf

--- api.py
+++ api.py
@@ -3,170 +3,182 @@ import pymysql
 import sys
 import traceback
 
-comp_url = "http://141.41.235.28/JSON_Kompressor_IPT/Kompressor_Json.php"
-response = requests.get(comp_url)
-data = response.json()
+#Neuen Error Log schreiben
+class Api:
 
-relevant_keys = {
-    "Kompressor_IPT": ["ID", "Zeitstempel", "Energie_gesamt_kwh"], #GeräteID 1
-    "Kompressor_IPT_Entlueftung": ["ID", "Zeitstempel", "Energie_gesamt_kwh"],  #GeräteID 2
-    "Kompressor_IPT_Kuehler": ["ID", "Zeitstempel", "Energie_gesamt_kwh"],  #GeräteID 3
-    "Kompressor_IPT_Sensoren": ["ID", "Zeitstempel", "Druck", "Durchfluss", "Temperatur1"], #SensorID 1
-    "Kompressor_Ostfalia": ["ID", "Zeitstempel", "Energie_gesamt_kwh"],  #GeräteID 4
-} #Geräte ID 0 ist für das Testing
-def get_relevant_data(api_response, relevant_keys):
-    relevant_data = {}
+    relevant_keys = {
+        "Kompressor_IPT": ["ID", "Zeitstempel", "Energie_gesamt_kwh"],
+        "Kompressor_IPT_Entlueftung": ["ID", "Zeitstempel", "Energie_gesamt_kwh"],
+        "Kompressor_IPT_Kuehler": ["ID", "Zeitstempel", "Energie_gesamt_kwh"],
+        "Kompressor_IPT_Sensoren": ["ID", "Zeitstempel", "Druck", "Durchfluss", "Temperatur1"],
+        "Kompressor_Ostfalia": ["ID", "Zeitstempel", "Energie_gesamt_kwh"],
+    }
+    def get_response_url(self, url):
+        response = requests.get(url)
+        return response.json()
 
-    for category, keys in relevant_keys.items():
-        if category in api_response:
-            relevant_data[category] = []
 
-            for entry in api_response[category]:
-                relevant_entry = {}
-                for key in keys:
-                    relevant_entry[key] = entry.get(key)
 
-                relevant_data[category].append(relevant_entry)
+    def get_relevant_data(self, api_response, relevant_keys):
+        relevant_data = {}
 
-    return relevant_data
+        for category, keys in relevant_keys.items():
+            if category in api_response:
+                relevant_data[category] = []
 
-def connect_to_database():
-    try:
-        connection = pymysql.connect(host='141.41.42.211',
-                                 user='Kompressor',
-                                 password='Kompressor12345%',
-                                 database='kompressor',
-                                 charset='utf8mb4',
-                                 cursorclass=pymysql.cursors.DictCursor)
-        return connection
-    except pymysql.Error as e:
-        print(f"Error connecting to the database: {e}")
-        return None
+                for entry in api_response[category]:
+                    relevant_entry = {}
+                    for key in keys:
+                        relevant_entry[key] = entry.get(key)
 
-def insert_test_data():
-    connection = connect_to_database()
-    try:
-        with connection.cursor() as cursor:
-            # Beispiel-Datensatz zum Einfügen
-            test_data = {
-                "ID_Geraet": 0,
-                "Bereich": "Test",
-                "Zeitstempel": "2023-01-01 12:00:00",
-                "Energie": 1000,
-                "Sensor": 0,
-            }
+                    relevant_data[category].append(relevant_entry)
 
-            # SQL-Anweisung zum Einfügen der Daten
-            sql = "INSERT INTO geraet (gerät_id, bereich, zeitstempel, energie, sensor_id) VALUES (%s, %s, %s, %s, %s)"
+        return relevant_data
 
-            cursor.execute(sql, (test_data["ID_Geraet"], test_data["Bereich"], test_data["Zeitstempel"], test_data["Energie"], test_data["Sensor"]))
-
-        connection.commit()
-    except pymysql.Error as e:
-        print(f"Error inserting data into the database: {e}")
-    finally:
-        # Schließe die Verbindung
-        connection.close()
-def delete_test_data():
-    connection = connect_to_database()
-
-    if connection:
+    def connect_to_database(self):
         try:
-            with connection.cursor() as cursor:
-                # SQL-Anweisung zum Löschen des Testdatensatzes
-                sql = "DELETE FROM geraet WHERE gerät_id = %s"
-
-                # Führe die SQL-Anweisung aus
-                cursor.execute(sql, 0)
-
-            # Commit, um die Änderungen in der Datenbank zu speichern
-            connection.commit()
-
+            connection = pymysql.connect(host='141.41.42.211',
+                                         user='Kompressor',
+                                         password='Kompressor12345%',
+                                         database='kompressor',
+                                         charset='utf8mb4',
+                                         cursorclass=pymysql.cursors.DictCursor)
+            return connection
         except pymysql.Error as e:
-            print(f"Error deleting data from the database: {e}")
-        finally:
-            # Schließe die Verbindung
-            connection.close()
+            print(f"Error connecting to the database: {e}")
+            return None
 
-def insert_data(relevant_data):
-    connection = connect_to_database()
-
-    if connection:
+    def insert_test_data(self):
+        connection = self.connect_to_database()
         try:
             with connection.cursor() as cursor:
-                for category, entries in relevant_data.items():
-                    for entry in entries:
-                        # Überprüfe, ob es sich um Sensoren handelt
-                        if category == "Kompressor_IPT_Sensoren":
-                            # SQL-Anweisung zum Einfügen der Sensordaten
-                            sql = "INSERT INTO sensor (datas_id, zeitstempel, druck, durchfluss, temperatur, sensor_id) VALUES (%s, %s, %s, %s, %s, %s)"
+                # Beispiel-Datensatz zum Einfügen
+                test_data = {
+                    "ID_Geraet": 0,
+                    "Bereich": "Test",
+                    "Zeitstempel": "2023-01-01 12:00:00",
+                    "Energie": 1000,
+                    "Sensor": 0,
+                }
 
-                            # Führe die SQL-Anweisung mit den Sensordaten aus
-                            cursor.execute(sql, (
-                                entry["ID"],
-                                entry["Zeitstempel"],
-                                entry["Druck"],
-                                entry["Durchfluss"],
-                                entry["Temperatur1"],
-                                1 # nur ein Sensor
-                            ))
-                        elif category == "Kompressor_IPT":
+                # SQL-Anweisung zum Einfügen der Daten
+                sql = "INSERT INTO geraet (gerät_id, bereich, zeitstempel, energie, sensor_id) VALUES (%s, %s, %s, %s, %s)"
 
-                            # Für alle anderen Kategorien (Geräte)
-                            # SQL-Anweisung zum Einfügen der Gerätedaten
-                            sql = "INSERT INTO geraet (datag_id, bereich, zeitstempel, energie, geraet_id, datas_id) VALUES (%s, %s, %s, %s, %s, %s)"
+                cursor.execute(sql, (
+                test_data["ID_Geraet"], test_data["Bereich"], test_data["Zeitstempel"], test_data["Energie"],
+                test_data["Sensor"]))
 
-                            # Führe die SQL-Anweisung mit den Gerätedaten aus
-                            cursor.execute(sql, (
-                                entry["ID"],
-                                category,
-                                entry["Zeitstempel"],
-                                entry["Energie_gesamt_kwh"],
-                                get_geraete_id(category),
-                                relevant_data["Kompressor_IPT_Sensoren"][0]["ID"]
-                            ))
-                        else:
-                            sql = "INSERT INTO geraet (datag_id, bereich, zeitstempel, energie, geraet_id) VALUES (%s, %s, %s, %s, %s)"
-
-                            # Führe die SQL-Anweisung mit den Gerätedaten aus
-                            cursor.execute(sql, (
-                                entry["ID"],
-                                category,
-                                entry["Zeitstempel"],
-                                entry["Energie_gesamt_kwh"],
-                                get_geraete_id(category)
-                            ))
-
-            # Commit, um die Änderungen in der Datenbank zu speichern
             connection.commit()
-
         except pymysql.Error as e:
             print(f"Error inserting data into the database: {e}")
         finally:
             # Schließe die Verbindung
             connection.close()
 
-def get_geraete_id(category):
-    if category == "Kompressor_IPT":
-        return 1
-    elif category == "Kompressor_IPT_Entlueftung":
-        return 2
-    elif category == "Kompressor_IPT_Kuehler":
-        return 3
-    elif category == "Kompressor_Ostfalia":
-        return 4
-    else:
-        return None
+    def delete_test_data(self):
+        connection = self.connect_to_database()
 
+        if connection:
+            try:
+                with connection.cursor() as cursor:
+                    # SQL-Anweisung zum Löschen des Testdatensatzes
+                    sql = "DELETE FROM geraet WHERE gerät_id = %s"
+
+                    # Führe die SQL-Anweisung aus
+                    cursor.execute(sql, 0)
+
+                # Commit, um die Änderungen in der Datenbank zu speichern
+                connection.commit()
+
+            except pymysql.Error as e:
+                print(f"Error deleting data from the database: {e}")
+            finally:
+                # Schließe die Verbindung
+                connection.close()
+
+    def insert_data(self, relevant_data):
+        connection = self.connect_to_database()
+
+        if connection:
+            try:
+                with connection.cursor() as cursor:
+                    for category, entries in relevant_data.items():
+                        for entry in entries:
+                            # Überprüfe, ob es sich um Sensoren handelt
+                            if category == "Kompressor_IPT_Sensoren":
+                                # SQL-Anweisung zum Einfügen der Sensordaten
+                                sql = "INSERT INTO sensor (datas_id, zeitstempel, druck, durchfluss, temperatur, sensor_id) VALUES (%s, %s, %s, %s, %s, %s)"
+
+                                # Führe die SQL-Anweisung mit den Sensordaten aus
+                                cursor.execute(sql, (
+                                    entry["ID"],
+                                    entry["Zeitstempel"],
+                                    entry["Druck"],
+                                    entry["Durchfluss"],
+                                    entry["Temperatur1"],
+                                    1  # nur ein Sensor
+                                ))
+                            elif category == "Kompressor_IPT":
+
+                                # Für alle anderen Kategorien (Geräte)
+                                # SQL-Anweisung zum Einfügen der Gerätedaten
+                                sql = "INSERT INTO geraet (datag_id, bereich, zeitstempel, energie, geraet_id, datas_id) VALUES (%s, %s, %s, %s, %s, %s)"
+
+                                # Führe die SQL-Anweisung mit den Gerätedaten aus
+                                cursor.execute(sql, (
+                                    entry["ID"],
+                                    category,
+                                    entry["Zeitstempel"],
+                                    entry["Energie_gesamt_kwh"],
+                                    self.get_geraete_id(category),
+                                    relevant_data["Kompressor_IPT_Sensoren"][0]["ID"]
+                                ))
+                            else:
+                                sql = "INSERT INTO geraet (datag_id, bereich, zeitstempel, energie, geraet_id) VALUES (%s, %s, %s, %s, %s)"
+
+                                # Führe die SQL-Anweisung mit den Gerätedaten aus
+                                cursor.execute(sql, (
+                                    entry["ID"],
+                                    category,
+                                    entry["Zeitstempel"],
+                                    entry["Energie_gesamt_kwh"],
+                                    self.get_geraete_id(category)
+                                ))
+
+                # Commit, um die Änderungen in der Datenbank zu speichern
+                connection.commit()
+
+            except pymysql.Error as e:
+                print(f"Error inserting data into the database: {e}")
+            finally:
+                # Schließe die Verbindung
+                connection.close()
+
+    def get_geraete_id(self, category):
+        if category == "Kompressor_IPT":
+            return 1
+        elif category == "Kompressor_IPT_Entlueftung":
+            return 2
+        elif category == "Kompressor_IPT_Kuehler":
+            return 3
+        elif category == "Kompressor_Ostfalia":
+            return 4
+        else:
+            return None
+
+    def main(self):
+        try:
+            url = "http://141.41.235.28/JSON_Kompressor_IPT/Kompressor_Json.php"
+            api_response = self.get_response_url(url)
+            relevant_data = self.get_relevant_data(api_response, self.relevant_keys)
+            # Füge die relevanten Daten in die Datenbank ein
+            self.insert_data(relevant_data)
+        except Exception as e:
+            with open('error_log.txt', 'a') as f:
+                f.write(f"Error: {str(e)}\n")
+                f.write(traceback.format_exc())
+            sys.exit(1)
 
 if __name__ == '__main__':
-    try:
-        relevant_data = get_relevant_data(data, relevant_keys)
-        # Füge die relevanten Daten in die Datenbank ein
-        insert_data(relevant_data)
-    except Exception as e:
-        with open('error_log.txt', 'a') as f:
-            f.write(f"Error: {str(e)}\n")
-            f.write(traceback.format_exc())
-        sys.exit(1)
-
+    api_instance = Api()
+    api_instance.main()
--- test_api.py
+++ test_api.py
@@ -1,4 +1,72 @@
 """
+
+from api import *
+from unittest.mock import patch, MagicMock
+
+# Fügen Sie hier Importe für Ihre Testdaten hinzu, wenn sie externe Daten verwenden
+
+@patch('api.requests.get')  # Mockt die 'requests.get'-Funktion, die für die API-Anfragen verwendet wird
+def test_api_request_successful(mock_get):
+    # Mock the response status code
+    mock_response = MagicMock()
+    mock_response.status_code = 200
+    mock_get.return_value = mock_response
+
+    # Test the function
+    assert mock_response.status_code == 200
+
+@patch('api.requests.get')
+def test_api_request_returns_json(mock_get):
+    # Mocken Sie die response-Variable
+    mock_response = MagicMock()
+    mock_response.headers = {'Content-Type': 'application/json'}
+    mock_get.return_value = mock_response
+
+    # Testen Sie die Funktion
+    assert api_request_returns_json()
+
+@patch('api.get_relevant_data')
+def test_get_relevant_data(mock_get_relevant_data):
+    # Mocken Sie die data-Variable und die relevant_keys-Variable
+    mock_data = {}  # Fügen Sie hier Ihre Testdaten hinzu
+    mock_relevant_keys = {}  # Fügen Sie hier Ihre Testdaten hinzu
+    mock_get_relevant_data.return_value = mock_data
+
+    # Testen Sie die Funktion
+    result = get_relevant_data(mock_data, mock_relevant_keys)
+
+    # Führen Sie Ihre vorhandenen Tests auf das Ergebnis durch
+
+@patch('api.connect_to_database')
+def test_connect_to_database(mock_connect_to_database):
+    # Mocken Sie die Verbindung zur Datenbank
+    mock_connection = MagicMock()
+    mock_connect_to_database.return_value = mock_connection
+
+    # Testen Sie die Funktion
+    connection = connect_to_database()
+
+    # Führen Sie Ihre vorhandenen Tests auf die 'connection'-Variable durch
+
+@patch('api.connect_to_database')
+@patch('api.insert_test_data')
+@patch('api.delete_test_data')
+def test_insert_data_into_database(mock_connect_to_database, mock_insert_test_data, mock_delete_test_data):
+    # Mocken Sie die Verbindung zur Datenbank
+    mock_connection = MagicMock()
+    mock_connect_to_database.return_value = mock_connection
+
+    # Testen Sie die Funktion
+    insert_data_into_database()
+
+    # Führen Sie Ihre vorhandenen Tests durch, die auf die Datenbank zugreifen
+
+
+"""
+
+
+
+"""
 Hinweise
 Eine Kopfzeile gehört überall hin / Es fehlen auch Kommentare
 
@@ -16,64 +84,137 @@ bankzugriffe könne sie virtuell ja nicht testen.
 
 """
 
+"""
+from api import *
+import unittest
+from unittest.mock import Mock, patch
+import json
+import responses
+
+class TestApi(unittest.TestCase):
+    def create_mock_response(self, status_code, data=None):
+        mock_response = Mock()
+        mock_response.status_code = status_code
+        mock_response.json.return_value = data  # Verwenden Sie json-Methode, um die Daten abzurufen
+        return mock_response
+
+    @patch('api.requests.get')
+    def test_api_request_successful(self, mock_get):
+        expected_data = {"Kompressor_IPT": [...]}
+
+        mock_response = self.create_mock_response(200, expected_data)
+        mock_get.return_value = mock_response
+
+        response = requests.get("http://141.41.235.28/JSON_Kompressor_IPT/Kompressor_Json.php")
+
+        self.assertIsNotNone(response)
+
+        self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(response.json(), expected_data)
+
+
+
+    @responses.activate
+    def test_api_request_returns_json(self):
+        url = "http://example.com/api"
+        responses.add(responses.GET, url, json={"some_key": "some_value"}, status=200,
+                      content_type='application/json')
+
+        response = requests.get(url)
+        assert response.headers['Content-Type'] == 'application/json'
+
+    def test_get_relevant_data(self):
+        # Erstellen Sie Mock-Daten für die API-Antwort
+        mock_data = {
+            "Kompressor_IPT": [{"ID": 1, "Zeitstempel": "2023-01-01", "Energie_gesamt_kwh": 100}],
+            "Kompressor_IPT_Entlueftung": [{"ID": 2, "Zeitstempel": "2023-01-01", "Energie_gesamt_kwh": 200}],
+        }
+
+        result = Api.get_relevant_data(mock_data, Api.relevant_keys)
+
+        # Überprüfen Sie, ob das Ergebnis ein Dictionary ist
+        assert isinstance(result, dict)
+
+        # Überprüfen Sie, ob jede Kategorie im Ergebnis ein Dictionary mit einer Liste von Dictionaries ist
+        for category, data_list in result.items():
+            assert isinstance(data_list, list)
+            assert all(isinstance(entry, dict) for entry in data_list)
+
+            # Überprüfen Sie, ob alle erwarteten Schlüssel in jedem Dictionary vorhanden sind
+            for entry in data_list:
+                assert all(key in entry for key in Api.relevant_keys[category])
+
+    def test_connect_to_database(self):
+        # Erstellen Sie einen Mock für die Datenbankverbindung
+        with unittest.mock.patch("pymysql.connect") as mock_connect:
+            # Setzen Sie die return_value des Mocks auf eine Instanz von pymysql.connections.Connection
+            mock_connection = pymysql.connections.Connection()
+            mock_connect.return_value = mock_connection
+
+            # Rufen Sie die Funktion zum Verbinden mit der Datenbank auf
+            connection = Api.connect_to_database()
+
+            # Überprüfen Sie, ob die Verbindung nicht None ist
+            assert connection is not None
+
+            # Überprüfen Sie, ob die Verbindung vom richtigen Typ ist
+            assert isinstance(connection, pymysql.connections.Connection)
+
+    def test_insert_data_into_database(self):
+        # Erstellen Sie Mock-Daten für die Datenbank
+        mock_relevant_data = {
+            "Kompressor_IPT": [{"ID": 1, "Zeitstempel": "2023-01-01", "Energie_gesamt_kwh": 100}],
+            "Kompressor_IPT_Entlueftung": [{"ID": 2, "Zeitstempel": "2023-01-01", "Energie_gesamt_kwh": 200}],
+        }
+
+        # Rufen Sie die Funktion zum Einfügen der Daten in die Datenbank auf
+        Api.insert_data(mock_relevant_data)
+
+        # Überprüfen Sie, ob die Daten in der Datenbank vorhanden sind
+        connection = Api.connect_to_database()
+
+        if connection:
+            try:
+                with connection.cursor() as cursor:
+                    # SQL-Anweisung zum Abfragen der eingefügten Datensätze
+                    sql = "SELECT * FROM geraet"
+
+                    # Führe die SQL-Anweisung aus
+                    cursor.execute(sql)
+
+                    # Überprüfen Sie, ob die Datensätze vorhanden sind
+                    result = cursor.fetchall()
+                    assert result is not None
+
+            finally:
+                # Schließe die Verbindung
+                connection.close()
+"""
 
 from api import *
+from unittest.mock import MagicMock, patch
 
-def test_api_request_successful():
-    assert response.status_code == 200
+class TestApi():
+    def test_api_call_successfull(self):
+        pass
 
-def test_api_request_returns_json():
-    assert response.headers['Content-Type'] == 'application/json'
+    def test_api_call_unsuccessfull(self):
+        pass
 
+    def test_api_call_returns_json(self):
+        pass
 
-def test_get_relevant_data():
+    def test_api_call_returns_no_json(self):
+        pass
+    def test_get_relevant_data(self):
+        pass
 
-    result = get_relevant_data(data, relevant_keys)
+    def test_did_not_get_relevant_data(self):
+        pass
 
-    # Überprüfe, ob das Ergebnis ein Dictionary ist
-    assert isinstance(result, dict)
+    def test_connection_to_database_successfull(self):
+        pass
 
-    # Überprüfe, ob jede Kategorie im Ergebnis ein Dictionary mit einer Liste von Dictionaries ist
-    for category, data_list in result.items():
-        assert isinstance(data_list, list)
-        assert all(isinstance(entry, dict) for entry in data_list)
-
-        # Überprüfe, ob alle erwarteten Schlüssel in jedem Dictionary vorhanden sind
-        for entry in data_list:
-            assert all(key in entry for key in relevant_keys[category])
-
-def test_connect_to_database():
-    # Call the function to connect to the database
-    connection = connect_to_database()
-
-    # Assert that the connection is not None
-    assert connection is not None
-
-    # Assert that the connection is of the correct type
-    assert isinstance(connection, pymysql.connections.Connection)
-
-def test_insert_data_into_database():
-    # Füge Testdaten zur Datenbank hinzu
-    insert_test_data()
-
-    # Verbinde erneut zur Datenbank, um die eingefügten Daten abzufragen
-    connection = connect_to_database()
-
-    if connection:
-        try:
-            with connection.cursor() as cursor:
-                # SQL-Anweisung zum Abfragen des eingefügten Datensatzes
-                sql = "SELECT * FROM gerät WHERE gerät_id = %s"
-
-                # Führe die SQL-Anweisung aus
-                cursor.execute(sql, 0)
-
-                # Überprüfe, ob der Datensatz vorhanden ist
-                result = cursor.fetchone()
-                assert result is not None
-
-        finally:
-            delete_test_data()
-            # Schließe die Verbindung
-            connection.close()
-
+    def test_connection_to_database_unsuccessfull(self):
+        pass
