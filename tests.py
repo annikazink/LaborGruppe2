cf490c7be7f489e6e242cff93687fe97494146b8
martenweiser <martenweiser@gmail.com>
2024-01-19 17:05:55 +0100

    Anpassung Tests für die GUI

--- tests.py
+++ tests.py
@@ -34,35 +34,6 @@ def test_bearbeite_datensaetze():
     assert result[0]['gesamt_energie'] == 80
     assert result[1]['gesamt_energie'] == 60
 
-"""
-def test_filter_data(self):
-    #Testet, ob die Filterfunktion der Daten korrekt funktioniert.
-    self.fail("Noch nicht implementiert")
-
-
-def test_display_data_in_line_graph(self):
-    #Testet, ob Daten korrekt in einem Liniendiagramm dargestellt werden.
-    self.fail("Noch nicht implementiert")
-
-
-def test_update_graph_on_data_change(self):
-    #Testet, ob der Graph sich aktualisiert, wenn sich die zugrunde liegenden Daten ändern.
-    self.fail("Noch nicht implementiert")
-
-
-def test_gui_response_to_invalid_data(self):
-    #Testet, ob die GUI korrekt auf ungültige Daten reagiert.
-    #Hier würden Sie die GUI-Funktion aufrufen, die ungültige Daten verarbeitet
-    #Dann überprüfen Sie, ob das Ergebnis Ihren Erwartungen entspricht
-    self.fail("Noch nicht implementiert")
-
-
-def test_error_message_display_for_missing_data(self):
-    #Testet, ob Fehlermeldungen korrekt angezeigt werden, wenn Daten fehlen.
-    #Hier würden Sie die GUI-Funktion aufrufen, die auf fehlende Daten reagiert
-    #Dann überprüfen Sie, ob eine angemessene Fehlermeldung angezeigt wird
-    self.fail("Noch nicht implementiert")
-"""
 
 import unittest
 import tkinter as tk
