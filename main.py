d97883ab560ce78186e1c353c71291304443ce01
martenweiser <martenweiser@gmail.com>
2024-01-21 12:51:43 +0100

    Anpassung Text

--- main.py
+++ main.py
@@ -2,19 +2,7 @@
 from gui_entwurf import MainApp
 
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    """print"""
-    # Use a breakpoint in the code line below to debug your script.
-    print(f"Hi, {name}")  # Press ⌘F8 to toggle the breakpoint.
-
-
 # Press the green button in the gutter to run the script.
 if __name__ == "__main__":
     app = MainApp()
     app.mainloop()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
