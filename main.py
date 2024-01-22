723348803945fc5fdff4f3fb7bbf7871a00ebe84
martenweiser <146163260+martenweiser@users.noreply.github.com>
2024-01-22 14:41:43 +0100

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-"""This is a sample Python script."""
-from gui_entwurf import MainApp
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == "__main__":
-    app = MainApp()
-    app.mainloop()
